68e7978b58e4dccc8eb9e5f98044e63839cf7e68
OnixcodeAB <onixcode.AB@gmail.com>
2023-05-24 21:25:34 -0400

    Configurando redux utlizando una version legacy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./reducers/store";
 import App from "./routes/App";
-
 //ReactDOM.render(<App />, document.getElementById("app"));
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<App tab="Home" />)
+root.render(
+  <Provider store={store}>
+    <App tab="Home" />
+  </Provider>
+);
--- src/routes/App.js
+++ src/routes/App.js
@@ -5,21 +5,17 @@ import Login from "../containers/Login";
 import Register from "../containers/Register";
 import Notfound from "../containers/Notfound";
 import Layout from "../containers/Layout";
-import { Provider } from "react-redux";
-import store from "../store";
 
 const App = () => (
   <BrowserRouter>
-    <Provider store={store}>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route component={Notfound} />
-        </Switch>
-      </Layout>
-    </Provider>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={Notfound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
